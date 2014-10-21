ca3fd1a1b47565462dd5ca56c90a1bee30f103d3
Róbert Viðar Bjarnason <robert@citizens.is>
2014-10-21 20:35:50 +0000

    Fix for editorview

--- support/client/lib/vwf/view/EditorView.js
+++ support/client/lib/vwf/view/EditorView.js
@@ -250,6 +250,7 @@ function InitializeEditor() {
     }
 
     require("vwf/view/editorview/InputSetup").initialize();
+    require("vwf/view/editorview/ChatSystemGUI").initialize();
 
     if (_EditorView.needTools()) {
         $('#sidepanel').css('height', $(window).height() - ($('#statusbar').height() + $('#toolbar').height() + $('#smoothmenu1').height()) + 'px')
@@ -258,7 +259,6 @@ function InitializeEditor() {
 
         require("vwf/view/editorview/Menubar").initialize();
         require("vwf/view/editorview/SideTabs").initialize();
-        require("vwf/view/editorview/ChatSystemGUI").initialize();
         $(document.head).append('<script type="text/javascript" src="vwf/view/localization/translate.js"></script>');
         translateMenu();
         //default to select mode
